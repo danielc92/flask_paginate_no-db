49fd9817b15cc1f0dc9b6917c02337070b813223
daniel corcoran <danielc.92g@gmail.com>
2018-09-26 12:25:41 +1000

    Added all files from flask_pagination testsearching branch

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from flask_paginate import Pagination, get_page_args
+import pandas
+
+app = Flask(__name__)
+app.debug = True
+
+data = pandas.read_csv('static/superstore.csv')
+data_dict = data.to_dict(orient = 'records')
+
+def get_users(offset=0, per_page=20):
+    return data_dict[offset: offset + per_page]
+
+@app.route('/')
+def index():
+
+    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
+    total = len(data_dict)
+    pagination_users = get_users(offset=offset, per_page=per_page)
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
+
+    return render_template('index.html',
+                           data=pagination_users,
+                           page=page,
+                           per_page=per_page,
+                           pagination=pagination)
